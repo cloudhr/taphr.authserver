ed80544f30645a6b9a8bb70aacca27b2b84b7dd4
Larry Eliemenye <denachural@gmail.com>
2016-05-21 19:33:18 +0100

    Adds support for running test

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+var gulp = require('gulp-help')(require('gulp'));
+
+gulp.task('run:test','Runs unit test for auth server', function(){
+  console.log('==================RAN TEST===========================')
+});
